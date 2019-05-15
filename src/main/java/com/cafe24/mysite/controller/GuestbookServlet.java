751383297e3f383777ddd668c1d835e94cceae75
BIT <BIT@DESKTOP-VC61ESU>
2019-05-15 13:46:59 +0900

    completed

--- src/main/java/com/cafe24/mysite/controller/GuestbookServlet.java
+++ src/main/java/com/cafe24/mysite/controller/GuestbookServlet.java
@@ -3,6 +3,7 @@ package com.cafe24.mysite.controller;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
